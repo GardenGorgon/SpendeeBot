3b3ffc1610cd7953f68e22f97cc5e0748a1c5e1e
Stephanie Martinez <stephaniemartinez@Stephanies-MacBook-Air.local>
2023-04-03 16:21:09 -0400

    Skeleton for Player and GameState

--- Player.py
+++ Player.py
@@ -7,16 +7,41 @@ has a number of points, set of chips, and set of cards they hold.
 
 At initialization, a player have 0 points and no cards/chips.
 """
+from Chip import Chip
 
 class Player():
-    def __init__(self, strategy):
+    
+    def __init__(self, strategy, points: int=0, chips: list[Chip]=[], cards=[], name: str = "Agent"):
+        self.name = name
         self.strategy = strategy
-        self.points = 0 
-        self.chips = []
-        self.cards = []
+        self.points = points 
+        self.chips = chips
+        self.cards = cards
 
+    def next_move(self, game_state):
+        # Returns the next move given the game_state
+        return self.strategy(game_state)
+    
+    def add_points(self, points):
+        # Adds points to this player's total points
+        self.points += points
+
+    # //////////////////// A couple of helpers
     def num_cards(self):
         return len(self.cards)
     
     def num_chips(self):
         return len(self.chips)
+    
+    def num_color_chip(self, color: str):
+        # returns the number of chips of a specific color
+        total_color = 0
+        for i in self.chips:
+            if i.color == color:
+                total_color += 1
+        return total_color
+    
+    def num_unique_chip(self):
+        # returns the number of unique chip colors
+        colors = [i.color for i in self.chips]
+        return len(set(colors))
--- SpendeeGameState.py
+++ SpendeeGameState.py
@@ -2,4 +2,64 @@
 CS4100
 
 This class represents the state of a game of Spendee. 
+Script moves for a player:
+
+grabChips [color] [number] [color] [number] ... until 5 chips are selected
+buyCard   [name?]
+buyNoble  [name?]
+help      displays instructions
+
 """
+from Player import Player
+
+commands = ['grabChips', 'buyCard', 'buyNoble', 'help']
+
+class SpendeeGameState():
+
+    def __init__(self, players: list[Player]=[], point_goal: int = 15):
+        """
+        players: the list of players in the game
+        point_goal: the points needed to win the game. Defaults to 15. 
+        TODO: Add a board param?
+        """
+        self.players = players
+        self.point_goal = point_goal
+
+    def play_game(self):
+        """
+        TODO: Plays the game. 
+        """
+        while (not self.is_game_over):
+            self.is_game_over = True
+            for player in self.players:
+               # successful_move = False
+               # while(not successful_move):
+               #    nextMove = player.strategy(self) 
+               #    successful_move = board(nextMove, player) # maybe return bool? and also changes board state
+               #    if (successful_move): display_action(nextMove) display_board()
+               return  
+        
+        for player in self.players:
+            if player.points == self.point_goal:
+                print(player.name + " won!")
+
+    def is_game_over(self):
+        """
+        Returns true if a player has reached the point goal.
+        """
+        for player in self.players:
+            if player.points == self.point_goal:
+                return True
+        return False
+    
+    def display_board(self):
+        # TODO: 
+        return 0
+    
+    def display_instructions(self):
+        # TODO: 
+        return ""
+    
+    def display_action(self, action):
+        # TODO: 
+        return ""
